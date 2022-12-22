157c984011852b544f3eb3531914770caee88949
pavanvenkatguttula <119720768+pavanvenkatguttula@users.noreply.github.com>
2022-12-22 10:23:12 +0530

    #include<stdio.h> int main() {     int n1,n2;     scanf("%d%d",&n1,&n2);     if(n2>n1)     {         printf("Profit");     }     else     {         printf("Loss");     } }

---
+++ Profit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    scanf("%d%d",&n1,&n2);
+    if(n2>n1)
+    {
+        printf("Profit");
+    }
+    else
+    {
+        printf("Loss");
+    }
+}
