6672b5d186f154ad6e3adf686ff1f07dcbac905e
pavanvenkatguttula <119720768+pavanvenkatguttula@users.noreply.github.com>
2022-12-29 10:35:11 +0530

    #include<stdio.h> int main() {     int n,m,i,c=0;     scanf("%d%d",&n,&m);     for(i=n;i<=m;i++)     {         if(i%2==0 && i%3==0)         {             c++;         }     }     printf("%d",c);
    }

---
+++ count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,c=0;
+    scanf("%d%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+        if(i%2==0 && i%3==0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+}
